bc26a29d711947f0fc57a1c2557c21368eb30813
github username <joshmrallen@gmail.com>
2020-07-08 17:23:46 -0400

    added prompt

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,25 @@
 # Add your code here
+
+# song names for our Jukebox to display
+song = [
+    "Phoenix - 1901",
+    "Tokyo Police Club - Wait Up",
+    "Sufjan Stevens - Too Much",
+    "The Naked and the Famous - Young Blood",
+    "(Far From) Home - Tiga",
+    "The Cults - Abducted",
+    "Phoenix - Consolation Prizes",
+    "Harry Chapin - Cats in the Cradle",
+    "Amos Lee - Keep It Loose, Keept It Tight"
+]
+
+
+#prompt user for their name
+def say_hello(name)
+    "Hi #{name}!"
+end
+
+puts "Enter your name:"
+users_name = gets.strip
+
+puts say_hello(users_name)
